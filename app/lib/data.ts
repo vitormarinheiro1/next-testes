847dc292badd29eb1b88e925bbf79b8c057033ec
Suporte <suporte@HBR.LOCAL>
2024-01-24 13:17:48 -0300

    Subindo dashboard completa

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -23,6 +23,8 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
+    
+
     // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
