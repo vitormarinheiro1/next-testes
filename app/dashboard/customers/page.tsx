03f9232b7546e0f1901013fed8604050960110a1
Suporte <suporte@HBR.LOCAL>
2024-01-25 10:35:26 -0300

    Adicionando título a customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+
 export default function Page() {
   return <p>Customers Page</p>
 }
+
+
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+};
